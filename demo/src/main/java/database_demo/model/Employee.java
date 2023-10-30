6ea52d6b17ac630d9e9ae259bb4e62e72242e0c4
Nelson <nelsonrafael.perezmonsanto@Student.SL.On.Ca>
2023-10-30 18:25:01 -0400

    Finish lab

--- demo/src/main/java/database_demo/model/Employee.java
+++ demo/src/main/java/database_demo/model/Employee.java
@@ -1,5 +1,25 @@
 package database_demo.model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@EntityScan
+@Data
 public class Employee {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    String userId;
+    String firstName;
+    String lastName;
+    String department;
+    String password;
+
+    
+
 }
